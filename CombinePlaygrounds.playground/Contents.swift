f450e21836f8dc29318b9727fa3166331609cd12
branch10480 <toshiharu.imaeda@gmail.com>
2022-04-04 21:46:19 +0900

    Update

--- CombinePlaygrounds.playground/Contents.swift
+++ CombinePlaygrounds.playground/Contents.swift
@@ -8,7 +8,7 @@ enum MyError: Error {
 var subject1 = PassthroughSubject<String, Never>()
 
 // sinkメソッドはsubscribeのこと
-// sinkはエラー型をNever(失敗しない）にしたときにだけ使える
+// sinkの1引数版はエラー型をNever(失敗しない）にしたときにだけ使える
 subject1
   .sink { value in
     print("Received value:", value)
@@ -24,6 +24,7 @@ print("--------------------------")
 
 let subject2 = PassthroughSubject<String, MyError>()
 
+// 2引数をとるsinkはエラー型が任意で使える
 subject2
   .sink(receiveCompletion: { completion in
     print("Received completion:", completion)
@@ -38,3 +39,41 @@ subject2.send("c")
 subject2.send(completion: .failure(.general))
 subject2.send("d")
 subject2.send("e")
+
+print("--------------------------")
+
+let subject3 = PassthroughSubject<String, Never>()
+
+class Receiver {
+  let subscription: AnyCancellable
+  
+  init() {
+    // この記述だと購読が解除される
+    // initのスコープ内でのみ参照可能のため、initが完了するとメモリ解放がされる
+//    subject3
+//      .sink(receiveCompletion: { completion in
+//        print("Received completion:", completion)
+//      }, receiveValue: { value in
+//        print("Received value:", value)
+//      })
+    
+    // sinkの返り値を保持すると購読状態が保たれる
+    // sinkはsubscriptionの一種
+    subscription = subject3
+      .sink(receiveCompletion: { completion in
+        print("Received completion:", completion)
+      }, receiveValue: { value in
+        print("Received value:", value)
+      })
+  }
+}
+
+print("出力される？")
+
+let receiver = Receiver()
+subject3.send("a")
+subject3.send("b")
+// AnyCancellableは購読キャンセル可能
+receiver.subscription.cancel()
+subject3.send("c")
+subject3.send(completion: .finished)
